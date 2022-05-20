032d54f26879271b30fdd09d00246504e6c0ead8
Katarina Bagaric <katarina.bagaric1235@gmail.com>
2022-05-20 17:20:14 +0200

    ADDING COMMENTS

--- yourinformation.js
+++ yourinformation.js
@@ -1,3 +1,4 @@
+//DECLARATING THE REFERENCE TO THE VALUES IN HTML
 const URL = window.location.search;
 const URLDATA = new URLSearchParams(URL);
 
@@ -6,16 +7,14 @@ const email_output = document.getElementById("email_id");
 const number_output = document.getElementById("number_id");
 const team_output = document.getElementById("team_id")
 
-
-
-
+//DECLARATING THE REFERENCE THE STORED VALUES IN STORAGE
 const name1 = sessionStorage.getItem("name");
 const email1 = sessionStorage.getItem("email");
 const number1 = sessionStorage.getItem("tel");
 const team1 = sessionStorage.getItem("desiredTeam");
 
 
-
+//OUTPUTS THE STORED VALUES INTO THE HTML
 name_output.innerHTML= name1
 email_output.innerHTML = email1
 number_output.innerHTML = number1
